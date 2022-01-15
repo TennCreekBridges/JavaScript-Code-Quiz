dcd8e9194bd176c003eb650a28b0b04bad1dce7d
TennCreekBridges <bridgettvalet@gmail.com>
2022-01-15 16:24:35 -0600

    trying to hide buttons at first

--- assets/js/script.js
+++ assets/js/script.js
@@ -50,67 +50,67 @@ const viewBtn = document.querySelector("#view-scores");
 const questions = [ 
     { question: "Inside which HTML element do we put the JavaScript?",
       answers: ["a. <javascript>", "b. <script>", "c. <js>", "d. <scripting>"],
-      correct: 2,
+      correctAnswer: 1,
       },
     {
       question: "How do you call a function named \"myFunction\"?",
       answers: ["a. call myFunction()", "b. myFunction ()", "c. call function MyFunction()", "d. function MyFunction"],
-      correct: 1,
+      correctAnswer: 1,
       },
     {
       question: "How do you write an IF statement in JavaScript?",
       answers: ["a. if i = 5 then", "b. if i => 5 then", "c. if i == 5 then", "d. if (i==5)"],
-      correct: 3,
+      correctAnswer: 3,
       },
     {
         question: "Write an IF statement for executing some code if \"i\" is NOT equal to 5?",
         answers: ["a. if i < > 5", "b. if (i < > 5)", "c. if(i != 5)", "d. if i=! 5 then"],
-        correct: 2,
+        correctAnswer: 2,
         },
     {
          question: "How does a WHILE loop start?",
          answers: ["a. while i = 1 to 10", "b. while (i <= 10)", "c. while(i <= 10; i++)", "d. while (i++ 10)"],
-        correct:  3,
+        correctAnswer:  3,
         },
     {
         question: "How does a FOR loop start?",
         answers: ["a. for (i = 0; i <= 5; i++)", "b. for (i <= 5; i++)", "c. for (i = 0; i <= 5)", "d. for i = 1 to 5"],
-        correct: 0,
+        correctAnswer: 0,
         },
     {
         question: "What is the correct way to write a JavaScript array?",
         answers: ["a. var colors = (1:\"red\", 2:\"green\", 3:\"blue\")", "b. var colors = 1 = (\"red\"), 2 = (\"green\"), 3 = (\"blue\")", "c. var colors = \"red\", \"green\", \"blue\"", "d. var colors=[\"red\", \"green\", \"blue\""],
-        correct: 3,
+        correctAnswer: 3,
         },
     {
         question: "How do you round the number 7.25 to the nearest integer?",
         answers: ["a. rnd(7.25)", "b. round(7.25)", "c. Math.rnd(7.25)", "d. Math.round(7.25)"],
-        correct: 3,
+        correctAnswer: 3,
         },
     {
         question: "How do you find the number with the highest value of x and y?",
         answers: ["a. Math.ceil(x,y)", "b. top(x,y)", "c. ceil(x,y)", "d. Math.max(x,y)"],
-        correct: 3,
+        correctAnswer: 3,
     },
     {
         question: "How can you detect the client's browser name?",
         answers: ["a. client.navName", "b. browser.name", "c. navigator.appName", "d. which.browser"],
-        correct: 2,
+        correctAnswer: 2,
         },
     {
         question: "Which event occurs when the user clicks on an HTML element?",
         answers: ["a. onmouseover", "b. onclick", "c. onmouseclick", "d. onchange"],
-        correct: 2,
+        correctAnswer: 2,
         },
     {
         question: "Which operator is used to assign a value to a variable?",
         answers: ["a. x", "b. =", "c. *", "d. -"],
-        correct: 1,
+        correctAnswer: 1,
         },
     {
         question: "What is the following code return: Boolean(10 > 9)",
         answers: ["a. true", "b. false", "c. NaN", "d. undefined"],
-            correct: 0,
+        correctAnswer: 0,
         },
 ]
 
@@ -124,7 +124,7 @@ function setTime() {
       if (secondsLeft === 0 || questionCount === questions.length) {
           clearInterval(timerInterval);
           questionsEl.style.display = "none";
-          finalEl.style.display = "block";
+          completeEl.style.display = "block";
           scoreEl.textContent = secondsLeft;
       }
   }, 1000);
@@ -159,13 +159,13 @@ function checkAnswer(event) {
   // remove message after one second
   setTimeout(function () {
     p.style.display = "none";
-  }, 1000);
+  }, 1500);
 
   if (questions[questionCount].correctAnswer === event.target.value) {
     p.textContent = "Correct!";
   } else if (questions[questionCount].correctAnswer !== event.target.value) {
-    secondsLeft = secondsLeft - 20;
-    p.textContent = "Incorrect. 20 Seconds Deducted."
+    secondsLeft = secondsLeft - 10;
+    p.textContent = "Incorrect. 10 Seconds Deducted."
   }
 
   if (questionCount < questions.length) {
