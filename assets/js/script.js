fb0f466239e9a679e92418f5fca2b1bc2ec670dc
TennCreekBridges <bridgettvalet@gmail.com>
2022-01-15 15:33:30 -0600

    added functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -35,39 +35,31 @@ const ansBtn = document.querySelectorAll("button.ansBtn");
 const ans1Btn = document.querySelector("#answer1");
 const ans2Btn = document.querySelector("#answer2");
 const ans3Btn = document.querySelector("#answer3");
-const ans4Btn = document.querySelector("answer4");
+const ans4Btn = document.querySelector("#answer4");
 // submit score
 const submitScrBtn = document.querySelector("#submit-score");
 // return
 const returnBtn = document.querySelector("#return");
+// go back
+const backBtn = document.querySelector("#go-back");
 // reset scores
-const resetBtn = document.querySelector("#resetscores");
+const resetBtn = document.querySelector("#reset-scores");
 // check scores
 const viewBtn = document.querySelector("#view-scores");
 
 const questions = [ 
     { question: "Inside which HTML element do we put the JavaScript?",
       answers: ["a. <javascript>", "b. <script>", "c. <js>", "d. <scripting>"],
-      correct: 1,
-      },
-    {
-      question: "Where is the correct place to insert JavaScript?",
-      answers: ["a. the <head> section", "b. the <body> section", "c. both the <head> section and <body> section are correct"],
-      correct: 2,
-      },
-    {
-      question: "What is the correct syntax for referring to an external script called \"xxx.js\"?",
-      answers: ["a. <script name=\"xxx.js\">", "b. <script href=\"xxx.js\">", "c. <script src=\"xxx.js\""],
       correct: 2,
       },
     {
       question: "How do you call a function named \"myFunction\"?",
-      answers: ["a. call myFunction()", "b. myFunction ()", "c. call function MyFunction()"],
+      answers: ["a. call myFunction()", "b. myFunction ()", "c. call function MyFunction()", "d. function MyFunction"],
       correct: 1,
       },
     {
       question: "How do you write an IF statement in JavaScript?",
-      answers: ["a. if i = 5 then", "b. if i = 5 then", "c. if i == 5 then", "d. if (i==5)"],
+      answers: ["a. if i = 5 then", "b. if i => 5 then", "c. if i == 5 then", "d. if (i==5)"],
       correct: 3,
       },
     {
@@ -77,8 +69,8 @@ const questions = [
         },
     {
          question: "How does a WHILE loop start?",
-         answers: ["a. while i = 1 to 10", "b. while (i <= 10)", "c. while(i <= 10; i++)"],
-        correct:  1,
+         answers: ["a. while i = 1 to 10", "b. while (i <= 10)", "c. while(i <= 10; i++)", "d. while (i++ 10)"],
+        correct:  3,
         },
     {
         question: "How does a FOR loop start?",
@@ -96,18 +88,13 @@ const questions = [
         correct: 3,
         },
     {
-        question: "How do you find the nuber with the highest value of x and y?",
+        question: "How do you find the number with the highest value of x and y?",
         answers: ["a. Math.ceil(x,y)", "b. top(x,y)", "c. ceil(x,y)", "d. Math.max(x,y)"],
         correct: 3,
-        },
-    {
-        question: "JavaScript is the same as Java.",
-        answers: ["a. True", "b. False"],
-        correct: 1,
-        },
+    },
     {
         question: "How can you detect the client's browser name?",
-        answers: ["a. client.navName", "b. browser.name", "c. navigator.appName"],
+        answers: ["a. client.navName", "b. browser.name", "c. navigator.appName", "d. which.browser"],
         correct: 2,
         },
     {
@@ -122,15 +109,150 @@ const questions = [
         },
     {
         question: "What is the following code return: Boolean(10 > 9)",
-        answers: ["a. true", "b. false", "c. NaN"],
+        answers: ["a. true", "b. false", "c. NaN", "d. undefined"],
             correct: 0,
-        },
-    {
-        question: "Is JavaScript case-sensitive?",
-        answers: ["a. Yes", "b. No"],
-        correct: 0,
         },
 ]
 
+// functions
+// begin quiz timer
+function setTime() {
+  let timerInterval = setInterval(function () {
+      secondsLeft--;
+      timeEl.textContent = `Time:${secondsLeft}s`;
 
+      if (secondsLeft === 0 || questionCount === questions.length) {
+          clearInterval(timerInterval);
+          questionsEl.style.display = "none";
+          finalEl.style.display = "block";
+          scoreEl.textContent = secondsLeft;
+      }
+  }, 1000);
+}
+
+function startQuiz() {
+  welcomeEl.style.display = "none";
+  questionsEl.style.display = "block";
+  questionCount = 0;
+  setTime();
+  setQuestions(questionCount);
+}
+
+// place questions, take count and display next
+function setQuestions(id) {
+  if (id < questions.length) {
+    questionEl.textContent = questions[id].question;
+    ans1Btn.textContent = questions[id].answers[0];
+    ans2Btn.textContent = questions[id].answers[1];
+    ans3Btn.textContent = questions[id].answers[2];
+    ans4Btn.textContent = questions[id].answers[3];
+  }
+}
+
+// check answer and move to next question
+function checkAnswer(event) {
+  event.preventDefault();
+  valid.style.display = "block";
+  let p = document.createElement("p");
+  valid.appendChild(p);
+
+  // remove message after one second
+  setTimeout(function () {
+    p.style.display = "none";
+  }, 1000);
+
+  if (questions[questionCount].correctAnswer === event.target.value) {
+    p.textContent = "Correct!";
+  } else if (questions[questionCount].correctAnswer !== event.target.value) {
+    secondsLeft = secondsLeft - 20;
+    p.textContent = "Incorrect. 20 Seconds Deducted."
+  }
+
+  if (questionCount < questions.length) {
+    questionCount++;
+  }
+
+  setQuestions(questionCount);
+}
+
+function addScore(event) {
+  event.preventDefault();
+  completeEl.style.display = "none";
+  highScoresEl.style.display = "block";
+  let init = initialsEl.value.toUpperCase();
+  scoreList.push({initials: init, score: secondsLeft});
+
+  // put scores in order
+  scoreList = scoreList.sort((a, b) => {
+    if (a.score < b.score) {
+      return 1;
+    } else {
+      return -1;
+    }
+  });
+
+  scoreListEl.innerHTML="";
+  for (let i=0; i < scoreList.length; i++) {
+    let li = document.createElement("li");
+    li.textContent = `${scoreList[i].initials}: ${scoreList[i].score}`;
+    scoreListEl.append(li);
+  }
+
+  // send to local storage
+  storeScores();
+  displayScores();
+}
+
+function storeScores() {
+  localStorage.setItem("scoreList", JSON.stringify(scoreList));
+}
+
+// get stored scores from local storage & parse string to an object
+function displayScores() {
+  let storedScoreList = JSON.parse(localStorage.getItem("scoreList"));
+  // uupdate score list array
+  if (storedScoreList !== null) {
+    scoreList = storedScoreList;
+  }
+}
+
+// reset scores
+function clearScores() {
+  localStorage.clear();
+  scoreListEl.innerHTML="";
+}
+
+// event listeners
+// start timer & show first question
+startBtn.addEventListener("click", startQuiz);
+
+// check answers
+ansBtn.forEach(item => {
+  item.addEventListener("click", checkAnswer);
+});
+
+// reset scores
+resetBtn.addEventListener("click", clearScores);
+
+// add score
+submitScrBtn.addEventListener("click", addScore);
+
+// go back
+backBtn.addEventListener("click", function () {
+  highScoresEl.style.display = "none";
+  welcomeEl.style.display = "block";
+  secondsLeft = 60;
+  timeEl.textContent = `Time:${secondsLeft}s`;
+});
+
+// view & hide high scores button
+viewBtn.addEventListener("click", function () {
+  if (highScoresEl.style.display === "none") {
+    highScoresEl.style.display = "block";
+  } else if (highScoresEl.style.display === "block") {
+    highScoresEl.style.display = "none";
+  } else {
+    return alert("No high scores yet.");
+  }
+});
 
