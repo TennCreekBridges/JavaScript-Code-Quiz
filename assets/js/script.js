b653e5e5da38a77742352b250a34ecca15f823e6
TennCreekBridges <bridgettvalet@gmail.com>
2022-01-15 17:57:30 -0600

    added quotes to answers (eyeroll)

--- assets/js/script.js
+++ assets/js/script.js
@@ -50,67 +50,67 @@ const viewBtn = document.querySelector("#view-scores");
 const questions = [ 
     { question: "Inside which HTML element do we put the JavaScript?",
       answers: ["a. <javascript>", "b. <script>", "c. <js>", "d. <scripting>"],
-      correctAnswer: 1,
+      correctAnswer: "1"
       },
     {
       question: "How do you call a function named \"myFunction\"?",
       answers: ["a. call myFunction()", "b. myFunction ()", "c. call function MyFunction()", "d. function MyFunction"],
-      correctAnswer: 1,
+      correctAnswer: "1"
       },
     {
       question: "How do you write an IF statement in JavaScript?",
       answers: ["a. if i = 5 then", "b. if i => 5 then", "c. if i == 5 then", "d. if (i==5)"],
-      correctAnswer: 3,
+      correctAnswer: "3"
       },
     {
         question: "Write an IF statement for executing some code if \"i\" is NOT equal to 5?",
         answers: ["a. if i < > 5", "b. if (i < > 5)", "c. if(i != 5)", "d. if i=! 5 then"],
-        correctAnswer: 2,
+        correctAnswer: "2"
         },
     {
          question: "How does a WHILE loop start?",
          answers: ["a. while i = 1 to 10", "b. while (i <= 10)", "c. while(i <= 10; i++)", "d. while (i++ 10)"],
-        correctAnswer:  3,
+        correctAnswer:  "3"
         },
     {
         question: "How does a FOR loop start?",
         answers: ["a. for (i = 0; i <= 5; i++)", "b. for (i <= 5; i++)", "c. for (i = 0; i <= 5)", "d. for i = 1 to 5"],
-        correctAnswer: 0,
+        correctAnswer: "0"
         },
     {
         question: "What is the correct way to write a JavaScript array?",
         answers: ["a. var colors = (1:\"red\", 2:\"green\", 3:\"blue\")", "b. var colors = 1 = (\"red\"), 2 = (\"green\"), 3 = (\"blue\")", "c. var colors = \"red\", \"green\", \"blue\"", "d. var colors=[\"red\", \"green\", \"blue\""],
-        correctAnswer: 3,
+        correctAnswer: "3"
         },
     {
         question: "How do you round the number 7.25 to the nearest integer?",
         answers: ["a. rnd(7.25)", "b. round(7.25)", "c. Math.rnd(7.25)", "d. Math.round(7.25)"],
-        correctAnswer: 3,
+        correctAnswer: "3"
         },
     {
         question: "How do you find the number with the highest value of x and y?",
         answers: ["a. Math.ceil(x,y)", "b. top(x,y)", "c. ceil(x,y)", "d. Math.max(x,y)"],
-        correctAnswer: 3,
+        correctAnswer: "3"
     },
     {
         question: "How can you detect the client's browser name?",
         answers: ["a. client.navName", "b. browser.name", "c. navigator.appName", "d. which.browser"],
-        correctAnswer: 2,
+        correctAnswer: "2"
         },
     {
         question: "Which event occurs when the user clicks on an HTML element?",
         answers: ["a. onmouseover", "b. onclick", "c. onmouseclick", "d. onchange"],
-        correctAnswer: 2,
+        correctAnswer: "2"
         },
     {
         question: "Which operator is used to assign a value to a variable?",
         answers: ["a. x", "b. =", "c. *", "d. -"],
-        correctAnswer: 1,
+        correctAnswer: "1"
         },
     {
         question: "What is the following code return: Boolean(10 > 9)",
         answers: ["a. true", "b. false", "c. NaN", "d. undefined"],
-        correctAnswer: 0,
+        correctAnswer: "0"
         },
 ]
 
@@ -121,7 +121,7 @@ function setTime() {
       secondsLeft--;
       timeEl.textContent = `Time:${secondsLeft}s`;
 
-      if (secondsLeft === 0 || questionCount === questions.length) {
+      if (secondsLeft <= 0 || questionCount === questions.length) {
           clearInterval(timerInterval);
           questionsEl.style.display = "none";
           completeEl.style.display = "block";
